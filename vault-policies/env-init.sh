ba4147c8d457f46e8d8b60f0694daaa1cf00d324
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-30 15:44:49 -0700

    Updated

--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -7,10 +7,10 @@ echo "Installing Vault..."
 wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
 
 unzip vault_${VAULT}_linux_amd64.zip
-sudo chmod +x vault
-sudo mv vault /usr/bin
-sudo chmod 0755 /usr/bin/vault
-sudo chown root:root /usr/bin/vault
+chmod +x vault
+mv vault /usr/bin
+chmod 0755 /usr/bin/vault
+chown root:root /usr/bin/vault
 
 echo "Starting the Vault server..."
 # Run Vault

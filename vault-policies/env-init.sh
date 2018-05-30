1540b6e9db219cf324162c665d62c973dc5bc465
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-30 14:46:22 -0700

    Updated

--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -1,3 +1,20 @@
-docker run vault:latest
+#!/usr/bin/env bash
 
-export VAULT_ADDR=http://127.0.0.1:8200
+# Get Vault
+VAULT=0.10.1
+VAULT_ZIP=/vagrant/bin/vault_${VAULT}_linux_amd64.zip
+mkdir -p /vagrant/bin
+wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip --quiet -O $VAULT_ZIP
+
+cd /tmp
+unzip -q $VAULT_ZIP >/dev/null
+sudo chmod +x vault
+sudo mv vault /usr/bin
+sudo chmod 0755 /usr/bin/vault
+sudo chown root:root /usr/bin/vault
+
+# Run Vault
+vault server -dev -dev-root-token-id="root"
+
+export VAULT_ADDR=http://localhost:8200
+vault login root

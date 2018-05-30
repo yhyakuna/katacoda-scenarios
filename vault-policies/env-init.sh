12e301bea27fa5d01ab2a9b782ad1f9c282edddd
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-30 15:41:05 -0700

    Updated

--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -2,16 +2,16 @@
 
 # Get Vault
 VAULT=0.10.1
-VAULT_ZIP=/vagrant/bin/vault_${VAULT}_linux_amd64.zip
-mkdir -p /vagrant/bin
-wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip --quiet -O $VAULT_ZIP
 
-cd /tmp
-unzip -q $VAULT_ZIP >/dev/null
+echo "Installing Vault..."
+wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
+
+unzip vault_${VAULT}_linux_amd64.zip
 sudo chmod +x vault
 sudo mv vault /usr/bin
 sudo chmod 0755 /usr/bin/vault
 sudo chown root:root /usr/bin/vault
 
+echo "Starting the Vault server..."
 # Run Vault
 vault server -dev -dev-root-token-id="root"

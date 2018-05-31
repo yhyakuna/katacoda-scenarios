d8c8f37cad296b5a29494d20d4712bbf0ce6d104
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-31 15:37:31 -0700

    Updated

--- vault-policies/docker-init.sh
+++ vault-policies/docker-init.sh
@@ -1,3 +1,1 @@
 docker run -d --name vault vault:latest
-
-alias vault='docker exec -it vault-dev vault "$@"'
--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
+docker run -d -p 8200:8200 -e VAULT_ADDR='http://127.0.0.1:8200' --name vault vault:latest
 
-# Get Vault
-export VAULT=0.10.1
-wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
-
-unzip vault_${VAULT}_linux_amd64.zip && rm vault_${VAULT}_linux_amd64.zip
-chmod +x vault
-mv vault /usr/bin
-chmod 0755 /usr/bin/vault
-chown root:root /usr/bin/vault
-
-rm vault_${VAULT}_linux_amd64.zip
-
-# Run Vault
-vault server -dev -dev-root-token-id="root"
+apt-get install -y jq

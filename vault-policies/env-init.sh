ebef7ce5b69d4edf51ed7b1a3810660984c16386
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-31 16:03:49 -0700

    Updated

--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -1,3 +1,11 @@
 docker run -d -p 8200:8200 -e VAULT_ADDR='http://127.0.0.1:8200' --name vault vault:latest
 
+docker logs vault > root_token.txt
+
+grep 'Root Token:' root-token.txt | awk '{print $NF}' > root_token.txt
+
+alias vault='docker exec -it vault vault "$@"'
+
+vault login $(cat root_token.txt)
+
 apt-get install -y jq

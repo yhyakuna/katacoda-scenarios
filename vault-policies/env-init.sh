87a042391ce0c33e8e3dcbc12058721428c28396
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-31 16:12:48 -0700

    Updated

--- vault-policies/env-init.sh
+++ vault-policies/env-init.sh
@@ -1,5 +1,5 @@
 docker run -d -p 8200:8200 -e VAULT_ADDR='http://127.0.0.1:8200' --name vault vault:latest
-
+wait $!
 docker logs vault > root_token.txt
 
 grep 'Root Token:' root-token.txt | awk '{print $NF}' > root_token.txt

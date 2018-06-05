e6b86757440029e2c2bfff3ee3d9d0863f11f2b2
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-05 11:53:42 -0700

    Updated

--- vault-dynamic-secrets/base.sh
+++ vault-dynamic-secrets/base.sh
@@ -1,1 +1,3 @@
-docker run --name postgres -e POSTGRES_PASSWORD=training -d postgres
+docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=training -d postgres
+
+docker exec -it postgres psql -U postgres -c 'CREATE DATABASE myapp;' &>/dev/null

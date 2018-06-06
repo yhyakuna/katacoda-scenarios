533abb16cf41cc399577c2fc9ea1d5a075c1d7cf
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-06 13:21:57 -0700

    Updated

--- vault-dynamic-secrets/env-init2.sh
+++ vault-dynamic-secrets/env-init2.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+apt-get install -y jq
+
 # Get Vault
 export VAULT=0.10.2
 wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
 
 unzip vault_${VAULT}_linux_amd64.zip && rm vault_${VAULT}_linux_amd64.zip
 install -c -m 0755 vault /usr/bin
+
+
 
 # Run Vault
 vault server -dev -dev-root-token-id="root" > /dev/null

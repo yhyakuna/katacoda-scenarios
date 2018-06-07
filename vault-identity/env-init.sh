e5f1c0e6986186d68534469f14b1971e0e311992
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-07 09:18:56 -0700

    Updated

--- vault-identity/env-init.sh
+++ vault-identity/env-init.sh
@@ -1,6 +1,3 @@
 docker run -d -p 8200:8200 -e VAULT_ADDR='http://127.0.0.1:8200' --name vault vault:latest
 
 alias vault='docker exec -it vault vault "$@"'
-
-sudo chmod 644 /var/lib/dpkg/status
-apt-get install -y jq

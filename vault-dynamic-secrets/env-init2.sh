6d544872b96b56305edf3ae037f4a456ee5de47e
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-19 12:33:37 -0700

    Updated

--- vault-dynamic-secrets/env-init2.sh
+++ vault-dynamic-secrets/env-init2.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip &&
+unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
 
 ssh root@host01 "curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip"
 ssh root@host01 "apt-get install jq -y"

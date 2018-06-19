434c27bf49607ed0ab0ac810d0e96ddc1431561b
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-19 12:38:42 -0700

    Updated

--- vault-dynamic-secrets/env-init2.sh
+++ vault-dynamic-secrets/env-init2.sh
@@ -2,4 +2,4 @@
 curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip &&
 unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
 
-ssh root@host01 "curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip"
+ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip"

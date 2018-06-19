26e9ab9e5d5b4a5b02f29e4a077361e6b1cee86c
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-19 12:23:45 -0700

    Updated

--- vault-dynamic-secrets/env-init2.sh
+++ vault-dynamic-secrets/env-init2.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
+curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip
+unzip -d ~/.bin ~/vault.zip
 
-# Get Vault
-export VAULT=0.10.2
-wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
+rm ~/vault.zip
 
-unzip vault_${VAULT}_linux_amd64.zip && rm vault_${VAULT}_linux_amd64.zip
-install -c -m 0755 vault /usr/bin
-
-apt-get install -y jq &&
-
-# Run Vault
-vault server -dev -dev-root-token-id="root" > /dev/null
+ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip"
+ssh root@host01 "apt-get install jq"

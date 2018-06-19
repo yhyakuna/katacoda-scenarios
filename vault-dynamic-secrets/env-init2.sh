e668ab014e4d08f444833b32997878f921317c8d
Yoko Hyakuna <yoko@hashicorp.com>
2018-06-19 12:30:59 -0700

    Updated

--- vault-dynamic-secrets/env-init2.sh
+++ vault-dynamic-secrets/env-init2.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip
-unzip -d ~/.bin ~/vault.zip
-
-rm ~/vault.zip
+curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_linux_amd64.zip &&
+unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip

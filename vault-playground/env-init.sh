41086557e625d143fd1183c13114b5becd3b8e10
Yoko Hyakuna <yoko@hashicorp.com>
2018-07-24 10:08:00 -0700

    Updated

--- vault-playground/env-init.sh
+++ vault-playground/env-init.sh
@@ -10,3 +10,6 @@ export VAULT_ADDR='http://127.0.0.1:8200'
 apt-get install -y jq
 
 ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq &$ export VAULT_ADDR='http://127.0.0.1:8200' &"
+
+
+vault server -dev -dev-root-token-id="root"

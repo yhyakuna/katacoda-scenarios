05bad29585b3cee263efcbc91902f2d0ca25b7f1
Yoko Hyakuna <yoko@hashicorp.com>
2019-01-11 15:28:40 -0800

    Updated

---
+++ vault-playground/assets/config.hcl
@@ -0,0 +1,11 @@
+ui = true
+disable_mlock = true
+
+storage "file" {
+  path = "/data/vault"
+}
+
+listener "tcp" {
+  address     = "0.0.0.0:8200"
+  tls_disable = 1
+}
--- vault-playground/env-init.sh
+++ vault-playground/env-init.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-curl -s -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip
-chmod +x ~/.bin/unzip
 
-curl -s -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.0.1/vault_1.0.1_linux_amd64.zip &&
-unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
+host_commands=(
+"curl -s -L http://assets.joinscrapbook.com/unzip -o ~/.bin/unzip"
+"chmod +x ~/.bin/unzip"
+"curl -s -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.0.1/vault_1.0.1_linux_amd64.zip -O"
+"unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip"
 
-apt-get install -y jq
+"apt-get install -y jq"
+"useradd vault --create-home"
+"mkdir -p /etc/vault.d"
+"cd /etc/vault.d && curl -L https://github.com/yhyakuna/katacoda-scenarios/tree/master/vault-playground/assets/config.hcl"
+"mkdir -p /home/vault/log"
+"chown -R vault /home/vault"
+"runuser -l vault -c \"vault server -config=/etc/vault.d/config.hcl >/home/vault/log/vault.log 2>&1 &\""
+)
 
-vault server -dev -dev-root-token-id="root"
+all_commands=$(awk -v sep=' && ' 'BEGIN{ORS=OFS="";for(i=1;i<ARGC;i++){print ARGV[i],ARGC-i-1?sep:""}}' "${host_commands[@]}")
 
-ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.0.1/vault_1.0.1_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq && vault server -dev -dev-root-token-id="root" "
+echo "$all_commands"
+
+ssh root@host01 "$all_commands"

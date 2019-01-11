350ebd27c306842eec61c3c54e2e294bd6073f1d
Yoko Hyakuna <yoko@hashicorp.com>
2019-01-11 15:14:21 -0800

    Updated

--- vault-playground/env-init.sh
+++ vault-playground/env-init.sh
@@ -7,6 +7,6 @@ unzip -d ~/.bin ~/vault.zip && rm ~/vault.zip
 
 apt-get install -y jq
 
-vault server -dev -dev-root-token-id="root" > ~/vault.log
+vault server -dev -dev-root-token-id="root"
 
 ssh root@host01 "curl -L http://assets.joinscrapbook.com/unzip -o /usr/bin/unzip && chmod +x /usr/bin/unzip && curl -L -o ~/vault.zip https://releases.hashicorp.com/vault/1.0.1/vault_1.0.1_linux_amd64.zip && unzip -d  /usr/bin/ ~/vault.zip && rm ~/vault.zip && apt-get install -y jq && vault server -dev -dev-root-token-id="root" "
